399d8a53fb9071a6f0c5bab44dfc2563e06cc18c
Arllesson Gomes <arllessonsilvaaa@gmail.com>
2025-06-26 20:40:06 -0300

    Comentando

--- src/routes/ScrollToTop.tsx
+++ src/routes/ScrollToTop.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+
+// Componente para scrollar pra cima.. react router nao faz isso nativamente.
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
